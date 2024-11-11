cfecbb817b309b27020dea1b5a09534c84e46b84
rikka <admin@rikka.cc>
2024-11-11 15:21:34 +0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM oven/bun:1 AS base
+WORKDIR /usr/src/app
+ENV registry-url "https://registry.npmmirror.com"
+
+FROM base AS install
+RUN mkdir -p /temp/dev
+COPY bunfig.toml package.json bun.lockb /temp/dev/
+RUN cd /temp/dev && bun install --frozen-lockfile
+
+# install with --production (exclude devDependencies)
+RUN mkdir -p /temp/prod
+COPY bunfig.toml package.json bun.lockb /temp/prod/
+RUN cd /temp/prod && bun install --frozen-lockfile --production
+
+FROM base AS prerelease
+COPY --from=install /temp/prod/node_modules node_modules
+COPY . .
+
+# [optional] tests & build
+ENV NODE_ENV=production
+RUN bun test
+RUN bun run build
+
+# copy production dependencies and source code into final image
+FROM base AS release
+COPY --from=install /temp/dev/node_modules node_modules
+COPY --from=prerelease /usr/src/app/dist/index.js ./dist/index.js
+COPY --from=prerelease /usr/src/app/package.json .
+
+# run the app
+USER bun
+EXPOSE 3000/tcp
+ENTRYPOINT [ "bun", "run", "start" ]
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,32 @@
 import { Elysia } from "elysia";
-
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+import { resolve } from "path"
+import { cors } from '@elysiajs/cors'
+import { readdirSync, existsSync } from "fs"
+import sharp from "sharp"
+const app = new Elysia().use(cors())
+  .get("/:width/:height", ({ params, set }) => {
+    set.headers = { "content-type": "image/webp" }
+    return getFile(params)
+  })
+  .get("/*", ({  set }) => {
+    set.headers = { "content-type": "image/webp" }
+    return getFile()
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
+
+async function getFile(size: { width?: string, height?: string } = {}) {
+  const { width = 200, height = 200 } = size;
+  const imagePath = resolve(process.cwd(), "./images");
+  if (existsSync(imagePath)) {
+    const imageList = readdirSync(imagePath);
+    const imageFile = Bun.file(resolve(imagePath, imageList[Math.floor(Math.random() * imageList.length)]));
+    const sharpImage = sharp(await imageFile.arrayBuffer());
+    return sharpImage.resize(Number(width), Number(height)).webp().toBuffer()
+  } else {
+    return ""
+  }
+}
